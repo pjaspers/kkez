b0e0077f082c5b925f85a81c4c53aa4c1a094cf0
pjaspers <piet@jaspe.rs>
2014-10-20 22:20:33 +0100

    Serving static assets

--- kkez.go
+++ kkez.go
@@ -2,7 +2,16 @@ package main
 
 import "net/http"
 import "log"
+import "fmt"
+import "html"
+// import "html/template"
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+	// Serve our static assets
+	// (Make sure to strip out the public, before `FileServer` sees it)
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
