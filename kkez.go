335339ef5f7568263de7f5a580d97bdab311c057
pjaspers <piet@jaspe.rs>
2014-10-20 23:45:33 +0100

    Using a layout system.

--- kkez.go
+++ kkez.go
@@ -7,6 +7,11 @@ import "html"
 import "path"
 import "html/template"
 
+type LayoutData struct {
+	Title string
+	Data interface{}
+}
+
 type Index struct {
 	Title string
 	Content string
@@ -21,14 +26,19 @@ func main() {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		templatePath := path.Join("templates", "index.html")
-		tmpl, err:= template.ParseFiles(templatePath)
+		layout := path.Join("templates", "layout.html")
+		parse := func(name string) (*template.Template, error) {
+			t := template.New("") //.Funcs(funcMap)
+			return t.ParseFiles(layout, path.Join("templates", name))
+		}
+		index, err:= parse("index.html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		index := Index{"Is het kaarten vandaag?", "Neeje", "Volgende is bij iemand"}
-		if err:= tmpl.Execute(w, index); err != nil {
+		var layoutData = LayoutData{Title: "Dinges"}
+		layoutData.Data = Index{"Is het kaarten vandaag?", "Neeje", "Volgende is bij iemand"}
+		if err:= index.ExecuteTemplate(w, "layout.html", layoutData); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
