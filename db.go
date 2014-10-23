618346abd5d425b80663fbcdb87cb91ff52ef508
pjaspers <piet@jaspe.rs>
2014-10-23 23:44:48 +0200

    Using a very basic DB

---
+++ db.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
+	"time"
+)
+
+func setupDatabase() {
+	os.Remove("./kkez.db")
+
+	db, err := sql.Open("sqlite3", "./kkez.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	create table events (id integer not null primary key, name varchar(255), date date);
+	delete from events;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+	log.Printf("Set up a database")
+	return
+}
+
+func allEvents() ([]Event) {
+	db, err := sql.Open("sqlite3", "./kkez.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	rows, err := db.Query("select id, name, date from events")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var events []Event
+	for rows.Next() {
+		var id int
+		var name string
+		var date time.Time
+		rows.Scan(&id, &name, &date)
+		events = append(events, Event{Id: id, Name: name, Date: date})
+	}
+	return events
+}
+func nextEvent(on time.Time) (event Event) {
+	db, err := sql.Open("sqlite3", "./kkez.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	var name string
+	var date time.Time
+	db.QueryRow("SELECT name, date from events where date >= ? LIMIT 1", on).Scan(&name, &date)
+	return Event{Name: name, Date: date}
+}
+
+func createEvent(name string, date time.Time) {
+	db, err := sql.Open("sqlite3", "./kkez.db")
+	defer db.Close()
+	var count int
+	db.QueryRow("SELECT COUNT(*) from events where name = ? and date = ?", name, date).Scan(&count)
+	log.Printf("Count: %d", count)
+	if count < 1 {
+		_, err = db.Exec("insert into events(name, date) values(?, ?)", name, date)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Saved the record")
+	} else {
+		log.Printf("Found the record")
+	}
+	return
+}
--- kkez.go
+++ kkez.go
@@ -20,6 +20,7 @@ type Index struct {
 }
 
 type Event struct {
+	Id int
 	Name string
 	Date time.Time
 }
@@ -29,22 +30,37 @@ func dateFromString(stamp string) time.Time {
 	return t
 }
 var events = []Event{
-	Event{Name: "Frits", Date: dateFromString("2013, 5, 31")},
-	Event{Name: "Dennis", Date: dateFromString("2013, 6, 28")},
-	Event{Name: "Stijn", Date: dateFromString("2013, 7, 26")},
-	Event{Name: "Dieter", Date: dateFromString("2013, 8, 30")},
-	Event{Name: "Kurt", Date: dateFromString("2013, 9, 27")},
-	Event{Name: "Peter", Date: dateFromString("2013, 10, 25")},
-	Event{Name: "Katrien", Date: dateFromString("2013, 11, 29")},
-	Event{Name: "Gijs", Date: dateFromString("2013, 12, 27")},
-	Event{Name: "Jan", Date: dateFromString("2014, 1, 31")},
-	Event{Name: "Piet", Date: dateFromString("2014, 2, 28")},
-	Event{Name: "Maarten", Date: dateFromString("2014, 3, 28")},
-	Event{Name: "Koen", Date: dateFromString("2014, 4, 25")},
-	Event{Name: "Pier", Date: dateFromString("2014, 5, 30")},
-	Event{Name: "Karin", Date: dateFromString("2014, 6, 27")},
-	Event{Name: "Simon", Date: dateFromString("2014, 7, 25")},
-	Event{Name: "Erwin", Date: dateFromString("2014, 8, 29")},
+	Event{Name: "Pier",						Date: dateFromString("2012, 2, 24")},
+	Event{Name: "Frits",					Date: dateFromString("2012, 3, 30")},
+	Event{Name: "Karin",					Date: dateFromString("2012, 4, 27")},
+	Event{Name: "Simon",					Date: dateFromString("2012, 5, 25")},
+	Event{Name: "Stijn",					Date: dateFromString("2012, 6, 22")},
+	Event{Name: "Dennis",					Date: dateFromString("2012, 7, 27")},
+	Event{Name: "Erwin",					Date: dateFromString("2012, 8, 31")},
+	Event{Name: "Kurt",						Date: dateFromString("2012, 9, 28")},
+	Event{Name: "Peter",					Date: dateFromString("2012, 10,	26")},
+	Event{Name: "Katrien",				Date: dateFromString("2012, 11,	30")},
+	Event{Name: "Gijs",						Date: dateFromString("2012, 12,	28")},
+	Event{Name: "Jan",						Date: dateFromString("2013, 1, 25")},
+	Event{Name: "Piet",						Date: dateFromString("2013, 2, 22")},
+	Event{Name: "Maarten",				Date: dateFromString("2013, 3, 29")},
+	Event{Name: "Koen",						Date: dateFromString("2013, 4, 26")},
+	Event{Name: "Frits",					Date: dateFromString("2013, 5, 31")},
+	Event{Name: "Dennis",					Date: dateFromString("2013, 6, 28")},
+	Event{Name: "Stijn",					Date: dateFromString("2013, 7, 26")},
+	Event{Name: "Dieter",					Date: dateFromString("2013, 8, 30")},
+	Event{Name: "Kurt",						Date: dateFromString("2013, 9, 27")},
+	Event{Name: "Peter",					Date: dateFromString("2013, 10, 25")},
+	Event{Name: "Katrien",				Date: dateFromString("2013, 11, 29")},
+	Event{Name: "Gijs",						Date: dateFromString("2013, 12, 27")},
+	Event{Name: "Jan",						Date: dateFromString("2014, 1, 31")},
+	Event{Name: "Piet",						Date: dateFromString("2014, 2, 28")},
+	Event{Name: "Maarten",				Date: dateFromString("2014, 3, 28")},
+	Event{Name: "Koen",						Date: dateFromString("2014, 4, 26")},
+	Event{Name: "Pier",						Date: dateFromString("2014, 5, 30")},
+	Event{Name: "Karin",					Date: dateFromString("2014, 6, 27")},
+	Event{Name: "Simon",					Date: dateFromString("2014, 7, 25")},
+	Event{Name: "Erwin",					Date: dateFromString("2014, 8, 29")},
 }
 
 type ByDate []Event
@@ -63,6 +79,12 @@ func nextCardMoment(sortedEvents []Event, date time.Time) (event Event, isToday 
 }
 
 func main() {
+	// setupDatabase()
+	for _, event := range events {
+		createEvent(event.Name, event.Date)
+	}
+	log.Printf("Event %s", nextEvent(dateFromString("2014, 12, 23")))
+	log.Printf("All events: %s", allEvents())
 	sort.Sort(ByDate(events))
 
 	// Serve our static assets
